2cfffb72313b5deea0ca17473f0b6f37765d4132
安忆 <i@anyi.in>
2024-01-28 17:16:12 +0800

    build: set esbuild platform to `node` to avoid overriding global variables

--- scripts/modules/build-esbuild_options.ts
+++ scripts/modules/build-esbuild_options.ts
@@ -36,8 +36,8 @@ const esbuildOptions = {
 	define: {
 		'process.env.NODE_ENV': '"production"',
 	},
-	format: 'cjs',
 	legalComments: 'inline',
+	platform: 'node',
 	plugins: [
 		CssModulesPlugin({
 			targets,
