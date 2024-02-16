5c49b80d59649e86be802b600dbf216556e2fb08
安忆 <i@anyi.in>
2024-02-16 21:48:22 +0800

    build: update dependencies

--- scripts/modules/utils/build-util.ts
+++ scripts/modules/utils/build-util.ts
@@ -138,7 +138,7 @@ const generateTransformOptions = (): typeof options => {
 				{
 					bugfixes: true, // FIXME: Remove when updating to Babel 8
 					corejs: {
-						version: PACKAGE.devDependencies['core-js'].match(/\d+(?:.\d+){0,2}/)?.[0] ?? '3.35',
+						version: PACKAGE.devDependencies['core-js'].match(/\d+(?:.\d+){0,2}/)?.[0] ?? '3.36',
 					},
 					exclude: ['web.dom-collections.for-each', 'web.dom-collections.iterator'],
 					include: [] as string[],
