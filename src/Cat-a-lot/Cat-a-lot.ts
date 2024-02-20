8e6123742062ea10c5e52f993f03340f206a393f
海屿有燕 <zorua@vip.qq.com>
2024-02-20 21:39:16 +0800

    feat: 尝试加入很多猫分类工具

---
+++ src/Cat-a-lot/Cat-a-lot.ts
@@ -0,0 +1,7 @@
+import {catALot} from './modules/core';
+import {extendJQueryPrototype} from './modules/extendJQueryPrototype';
+
+/*! jQuery checkboxShiftClick | GPL-2.0 <https://qwbk.cc/H:GPL> */
+extendJQueryPrototype();
+/*! Cat-a-lot | CC-BY-SA-4.0 <https://qwbk.cc/H:CC-BY-SA-4.0> */
+catALot();
