9fe0abeabd08ee9b4c8ceb48695d84665c9982f0
白给 <2271007263A@gmail.com>
2024-02-20 11:54:27 +0800

    feat: add NavbarAvatar and Scroller

--- dist/InPageEdit/InPageEdit.js
+++ dist/InPageEdit/InPageEdit.js
@@ -31,7 +31,7 @@
  */
 /* <nowiki> */
 
-(function() {
+(() => {
 
 "use strict";
 
---
+++ src/NavbarAvatar/NavbarAvatar.ts
@@ -0,0 +1,14 @@
+$(() => {
+	let username = mw.user.getName();
+	let imgsrc = '';
+	if (username) {
+		username = username.replace(' ', '_'); //避免名称带空格时无法显示头像
+		imgsrc = `https://youshou.wiki/extensions/Avatar/avatar.php?user=${username}`;
+	} else {
+		imgsrc = 'https://youshou.wiki/images/avatars/default/default.gif';
+	}
+	const $body: JQuery<HTMLBodyElement> = $('body');
+	$body.find('#citizen-userMenu__buttonCheckbox .mw-ui-icon-wikimedia-userAvatar').css({
+		'background-image': `url(${imgsrc})`,
+	});
+});
