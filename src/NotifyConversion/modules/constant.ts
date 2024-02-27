783f29ca041972e7595f8dae8e3270f1dc5aa07e
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2024-02-27 13:29:15 +0800

    update: 上游源代码更新

--- src/NotifyConversion/modules/constant.ts
+++ src/NotifyConversion/modules/constant.ts
@@ -14,10 +14,10 @@ const SYSTEM_SCRIPT_LIST: string[] = [
 
 const WEBMASTER_LIST: string[] = ['QiuWen', 'QiuWen for Accounts', 'QiuwenSocialMedia'];
 
-const wgUserName: string = mw.config.get('wgUserName') || '';
-const wgUserGroups: string[] = mw.config.get('wgUserGroups') || [];
-const wgUserVariant: string | null = mw.config.get('wgUserVariant');
+const WG_USER_NAME: string = mw.config.get('wgUserName') || '';
+const WG_USER_GROUPS: string[] = mw.config.get('wgUserGroups') || [];
+const WG_USER_VARIANT: string | null = mw.config.get('wgUserVariant');
 
 const GOOD_VARIANTS: string[] = ['zh-cn', 'zh-my', 'zh-sg', 'zh-hk', 'zh-mo', 'zh-tw'];
 
-export {GADGET_NAME, SYSTEM_SCRIPT_LIST, WEBMASTER_LIST, wgUserName, wgUserGroups, wgUserVariant, GOOD_VARIANTS};
+export {GADGET_NAME, SYSTEM_SCRIPT_LIST, WEBMASTER_LIST, WG_USER_NAME, WG_USER_GROUPS, WG_USER_VARIANT, GOOD_VARIANTS};
