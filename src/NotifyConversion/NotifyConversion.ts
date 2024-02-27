6aed4495aac35fa0b2a9d40ba17829c0980fb62f
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2024-02-27 13:32:33 +0800

    Update NotifyConversion.ts

--- src/NotifyConversion/NotifyConversion.ts
+++ src/NotifyConversion/NotifyConversion.ts
@@ -1,4 +1,5 @@
-import {GADGET_NAME, GOOD_VARIANTS, wgUserName} from './modules/constant';
+import './NotifyConversion.less';
+import {GADGET_NAME, GOOD_VARIANTS, WG_USER_NAME} from './modules/constant';
 import {notifyConversion} from './modules/core';
 import {preserveVariant} from './modules/preserveVariant';
 
@@ -10,7 +11,7 @@ import {preserveVariant} from './modules/preserveVariant';
 	if (!['hide', ...GOOD_VARIANTS].includes(preferredVariant)) {
 		// 是否点过“不再提示”或完成过设置
 		$(notifyConversion);
-	} else if (!wgUserName) {
+	} else if (!WG_USER_NAME) {
 		$(preserveVariant);
 	}
 })();
