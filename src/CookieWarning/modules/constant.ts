446ed5d7a2af16432a14ad089daefda68e14f167
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2024-02-28 18:41:43 +0800

    refactor: 依据求闻上游更改结构

--- src/CookieWarning/modules/constant.ts
+++ src/CookieWarning/modules/constant.ts
@@ -1,5 +1,3 @@
-const CLASS_NAME: string = 'gadget-cookie_warning';
-
 const STORAGE_KEY: string = 'ext.gadget.CookieWarning_cookieConsent';
 
 const LAST_STORAGE_VALUE: string | null = mw.storage.get(STORAGE_KEY) as string | null;
@@ -8,4 +6,4 @@ const URL_CONSENT_READ: string | null = mw.util.getParamValue('consentRead');
 
 const WG_USER_NAME: string | null = mw.config.get('wgUserName');
 
-export {CLASS_NAME, LAST_STORAGE_VALUE, STORAGE_KEY, URL_CONSENT_READ, WG_USER_NAME};
+export {LAST_STORAGE_VALUE, STORAGE_KEY, URL_CONSENT_READ, WG_USER_NAME};
