1d347d7d0432f53896528c98921ca216f12519f0
海屿有燕 <zorua@vip.qq.com>
2024-03-11 12:06:56 +0800

    fix: use wgserver instead of fixed url

--- src/NavbarAvatar/NavbarAvatar.ts
+++ src/NavbarAvatar/NavbarAvatar.ts
@@ -2,10 +2,10 @@ import './NavbarAvatar.less';
 let username = mw.user.getName();
 let imgsrc = '';
 if (username) {
-	username = username.replace(' ', '_'); //避免名称带空格时无法显示头像
-	imgsrc = `https://youshou.wiki/extensions/Avatar/avatar.php?user=${username}`;
+	username = username.replace(' ', '_'); // 避免名称包含空格时无法显示头像
+	imgsrc = `${mw.config.get('wgServer')}/extensions/Avatar/avatar.php?user=${username}`;
 } else {
-	imgsrc = 'https://youshou.wiki/images/avatars/default/default.gif';
+	imgsrc = `${mw.config.get('wgServer')}/images/avatars/default/default.gif`;
 }
 const $body: JQuery<HTMLBodyElement> = $('body');
 $body.find('#citizen-userMenu__buttonCheckbox .mw-ui-icon-wikimedia-userAvatar').css({
