AnYiEE/AwesomeGadgets

commit c3b5d000a6e4e409978ff1791e697b3a392f3958

feat: add alt attribute to user avatars in UserLinkAvatar

dist/UserLinkAvatar/UserLinkAvatar.js
@@ -29,6 +29,7 @@ document.querySelectorAll(".mw-userlink").forEach((item) => {
   const img = document.createElement("img");
   img.classList.add("userlink-avatar");
   img.src = "".concat(mw.config.get("wgScriptPath"), "/extensions/Avatar/avatar.php?user=").concat(item.textContent);
+  img.alt = "Avatar of ".concat(item.textContent);
   item.prepend(img);
 });
 
@@ -36,4 +37,4 @@ document.querySelectorAll(".mw-userlink").forEach((item) => {
 
 /* </nowiki> */
 
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsic3JjL1VzZXJMaW5rQXZhdGFyL1VzZXJMaW5rQXZhdGFyLnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJpbXBvcnQgJy4vVXNlckxpbmtBdmF0YXIubGVzcyc7XG4vKiBlc2xpbnQtZGlzYWJsZSB1bmljb3JuL25vLWFycmF5LWZvci1lYWNoICovXG5kb2N1bWVudC5xdWVyeVNlbGVjdG9yQWxsKCcubXctdXNlcmxpbmsnKS5mb3JFYWNoKChpdGVtKSA9PiB7XG5cdGNvbnN0IGltZyA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2ltZycpO1xuXHRpbWcuY2xhc3NMaXN0LmFkZCgndXNlcmxpbmstYXZhdGFyJyk7XG5cdGltZy5zcmMgPSBgJHttdy5jb25maWcuZ2V0KCd3Z1NjcmlwdFBhdGgnKX0vZXh0ZW5zaW9ucy9BdmF0YXIvYXZhdGFyLnBocD91c2VyPSR7aXRlbS50ZXh0Q29udGVudH1gO1xuXHRpdGVtLnByZXBlbmQoaW1nKTtcbn0pO1xuIl0sCiAgIm1hcHBpbmdzIjogIjs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBRUFBLFNBQVNDLGlCQUFpQixjQUFjLEVBQUVDLFFBQVNDLFVBQVM7QUFDM0QsUUFBTUMsTUFBTUosU0FBU0ssY0FBYyxLQUFLO0FBQ3hDRCxNQUFJRSxVQUFVQyxJQUFJLGlCQUFpQjtBQUNuQ0gsTUFBSUksTUFBQSxHQUFBQyxPQUFTQyxHQUFHQyxPQUFPQyxJQUFJLGNBQWMsR0FBQyxxQ0FBQSxFQUFBSCxPQUFzQ04sS0FBS1UsV0FBVztBQUNoR1YsT0FBS1csUUFBUVYsR0FBRztBQUNqQixDQUFDOyIsCiAgIm5hbWVzIjogWyJkb2N1bWVudCIsICJxdWVyeVNlbGVjdG9yQWxsIiwgImZvckVhY2giLCAiaXRlbSIsICJpbWciLCAiY3JlYXRlRWxlbWVudCIsICJjbGFzc0xpc3QiLCAiYWRkIiwgInNyYyIsICJjb25jYXQiLCAibXciLCAiY29uZmlnIiwgImdldCIsICJ0ZXh0Q29udGVudCIsICJwcmVwZW5kIl0KfQo=
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsic3JjL1VzZXJMaW5rQXZhdGFyL1VzZXJMaW5rQXZhdGFyLnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJpbXBvcnQgJy4vVXNlckxpbmtBdmF0YXIubGVzcyc7XG5cbi8qIGVzbGludC1kaXNhYmxlIHVuaWNvcm4vbm8tYXJyYXktZm9yLWVhY2ggKi9cbmRvY3VtZW50LnF1ZXJ5U2VsZWN0b3JBbGwoJy5tdy11c2VybGluaycpLmZvckVhY2goKGl0ZW0pID0+IHtcblx0Y29uc3QgaW1nID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnaW1nJyk7XG5cdGltZy5jbGFzc0xpc3QuYWRkKCd1c2VybGluay1hdmF0YXInKTtcblx0aW1nLnNyYyA9IGAke213LmNvbmZpZy5nZXQoJ3dnU2NyaXB0UGF0aCcpfS9leHRlbnNpb25zL0F2YXRhci9hdmF0YXIucGhwP3VzZXI9JHtpdGVtLnRleHRDb250ZW50fWA7XG5cdGltZy5hbHQgPSBgQXZhdGFyIG9mICR7aXRlbS50ZXh0Q29udGVudH1gOyAvLyBhbHRcblx0aXRlbS5wcmVwZW5kKGltZyk7XG59KTtcbiJdLAogICJtYXBwaW5ncyI6ICI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUdBQSxTQUFTQyxpQkFBaUIsY0FBYyxFQUFFQyxRQUFTQyxVQUFTO0FBQzNELFFBQU1DLE1BQU1KLFNBQVNLLGNBQWMsS0FBSztBQUN4Q0QsTUFBSUUsVUFBVUMsSUFBSSxpQkFBaUI7QUFDbkNILE1BQUlJLE1BQUEsR0FBQUMsT0FBU0MsR0FBR0MsT0FBT0MsSUFBSSxjQUFjLEdBQUMscUNBQUEsRUFBQUgsT0FBc0NOLEtBQUtVLFdBQVc7QUFDaEdULE1BQUlVLE1BQUEsYUFBQUwsT0FBbUJOLEtBQUtVLFdBQVc7QUFDdkNWLE9BQUtZLFFBQVFYLEdBQUc7QUFDakIsQ0FBQzsiLAogICJuYW1lcyI6IFsiZG9jdW1lbnQiLCAicXVlcnlTZWxlY3RvckFsbCIsICJmb3JFYWNoIiwgIml0ZW0iLCAiaW1nIiwgImNyZWF0ZUVsZW1lbnQiLCAiY2xhc3NMaXN0IiwgImFkZCIsICJzcmMiLCAiY29uY2F0IiwgIm13IiwgImNvbmZpZyIsICJnZXQiLCAidGV4dENvbnRlbnQiLCAiYWx0IiwgInByZXBlbmQiXQp9Cg==

src/UserLinkAvatar/UserLinkAvatar.ts
@@ -1,8 +1,10 @@
 import './UserLinkAvatar.less';
+
 /* eslint-disable unicorn/no-array-for-each */
 document.querySelectorAll('.mw-userlink').forEach((item) => {
 	const img = document.createElement('img');
 	img.classList.add('userlink-avatar');
 	img.src = `${mw.config.get('wgScriptPath')}/extensions/Avatar/avatar.php?user=${item.textContent}`;
+	img.alt = `Avatar of ${item.textContent}`; // alt
 	item.prepend(img);
 });

commit 6dc87cbb229c0fbe737d66e08d28a070182dac26

fix(UserLinkAvatar): remove unnecessary alt text for avatar images
Fixes: c3b5d00

dist/UserLinkAvatar/UserLinkAvatar.js
@@ -29,7 +29,7 @@ document.querySelectorAll(".mw-userlink").forEach((item) => {
   const img = document.createElement("img");
   img.classList.add("userlink-avatar");
   img.src = "".concat(mw.config.get("wgScriptPath"), "/extensions/Avatar/avatar.php?user=").concat(item.textContent);
-  img.alt = "Avatar of ".concat(item.textContent);
+  img.alt = "";
   item.prepend(img);
 });
 
@@ -37,4 +37,4 @@ document.querySelectorAll(".mw-userlink").forEach((item) => {
 
 /* </nowiki> */
 
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsic3JjL1VzZXJMaW5rQXZhdGFyL1VzZXJMaW5rQXZhdGFyLnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJpbXBvcnQgJy4vVXNlckxpbmtBdmF0YXIubGVzcyc7XG5cbi8qIGVzbGludC1kaXNhYmxlIHVuaWNvcm4vbm8tYXJyYXktZm9yLWVhY2ggKi9cbmRvY3VtZW50LnF1ZXJ5U2VsZWN0b3JBbGwoJy5tdy11c2VybGluaycpLmZvckVhY2goKGl0ZW0pID0+IHtcblx0Y29uc3QgaW1nID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnaW1nJyk7XG5cdGltZy5jbGFzc0xpc3QuYWRkKCd1c2VybGluay1hdmF0YXInKTtcblx0aW1nLnNyYyA9IGAke213LmNvbmZpZy5nZXQoJ3dnU2NyaXB0UGF0aCcpfS9leHRlbnNpb25zL0F2YXRhci9hdmF0YXIucGhwP3VzZXI9JHtpdGVtLnRleHRDb250ZW50fWA7XG5cdGltZy5hbHQgPSBgQXZhdGFyIG9mICR7aXRlbS50ZXh0Q29udGVudH1gOyAvLyBhbHRcblx0aXRlbS5wcmVwZW5kKGltZyk7XG59KTtcbiJdLAogICJtYXBwaW5ncyI6ICI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUdBQSxTQUFTQyxpQkFBaUIsY0FBYyxFQUFFQyxRQUFTQyxVQUFTO0FBQzNELFFBQU1DLE1BQU1KLFNBQVNLLGNBQWMsS0FBSztBQUN4Q0QsTUFBSUUsVUFBVUMsSUFBSSxpQkFBaUI7QUFDbkNILE1BQUlJLE1BQUEsR0FBQUMsT0FBU0MsR0FBR0MsT0FBT0MsSUFBSSxjQUFjLEdBQUMscUNBQUEsRUFBQUgsT0FBc0NOLEtBQUtVLFdBQVc7QUFDaEdULE1BQUlVLE1BQUEsYUFBQUwsT0FBbUJOLEtBQUtVLFdBQVc7QUFDdkNWLE9BQUtZLFFBQVFYLEdBQUc7QUFDakIsQ0FBQzsiLAogICJuYW1lcyI6IFsiZG9jdW1lbnQiLCAicXVlcnlTZWxlY3RvckFsbCIsICJmb3JFYWNoIiwgIml0ZW0iLCAiaW1nIiwgImNyZWF0ZUVsZW1lbnQiLCAiY2xhc3NMaXN0IiwgImFkZCIsICJzcmMiLCAiY29uY2F0IiwgIm13IiwgImNvbmZpZyIsICJnZXQiLCAidGV4dENvbnRlbnQiLCAiYWx0IiwgInByZXBlbmQiXQp9Cg==
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsic3JjL1VzZXJMaW5rQXZhdGFyL1VzZXJMaW5rQXZhdGFyLnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJpbXBvcnQgJy4vVXNlckxpbmtBdmF0YXIubGVzcyc7XG5cbi8qIGVzbGludC1kaXNhYmxlIHVuaWNvcm4vbm8tYXJyYXktZm9yLWVhY2ggKi9cbmRvY3VtZW50LnF1ZXJ5U2VsZWN0b3JBbGwoJy5tdy11c2VybGluaycpLmZvckVhY2goKGl0ZW0pID0+IHtcblx0Y29uc3QgaW1nID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnaW1nJyk7XG5cdGltZy5jbGFzc0xpc3QuYWRkKCd1c2VybGluay1hdmF0YXInKTtcblx0aW1nLnNyYyA9IGAke213LmNvbmZpZy5nZXQoJ3dnU2NyaXB0UGF0aCcpfS9leHRlbnNpb25zL0F2YXRhci9hdmF0YXIucGhwP3VzZXI9JHtpdGVtLnRleHRDb250ZW50fWA7XG5cdGltZy5hbHQgPSAnJzsgLy8gYWx0PVwiXCIgZm9yIGRlY29yYXRpdmUgaW1hZ2VzXG5cdGl0ZW0ucHJlcGVuZChpbWcpO1xufSk7XG4iXSwKICAibWFwcGluZ3MiOiAiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7QUFHQUEsU0FBU0MsaUJBQWlCLGNBQWMsRUFBRUMsUUFBU0MsVUFBUztBQUMzRCxRQUFNQyxNQUFNSixTQUFTSyxjQUFjLEtBQUs7QUFDeENELE1BQUlFLFVBQVVDLElBQUksaUJBQWlCO0FBQ25DSCxNQUFJSSxNQUFBLEdBQUFDLE9BQVNDLEdBQUdDLE9BQU9DLElBQUksY0FBYyxHQUFDLHFDQUFBLEVBQUFILE9BQXNDTixLQUFLVSxXQUFXO0FBQ2hHVCxNQUFJVSxNQUFNO0FBQ1ZYLE9BQUtZLFFBQVFYLEdBQUc7QUFDakIsQ0FBQzsiLAogICJuYW1lcyI6IFsiZG9jdW1lbnQiLCAicXVlcnlTZWxlY3RvckFsbCIsICJmb3JFYWNoIiwgIml0ZW0iLCAiaW1nIiwgImNyZWF0ZUVsZW1lbnQiLCAiY2xhc3NMaXN0IiwgImFkZCIsICJzcmMiLCAiY29uY2F0IiwgIm13IiwgImNvbmZpZyIsICJnZXQiLCAidGV4dENvbnRlbnQiLCAiYWx0IiwgInByZXBlbmQiXQp9Cg==

src/UserLinkAvatar/UserLinkAvatar.ts
@@ -5,6 +5,6 @@ document.querySelectorAll('.mw-userlink').forEach((item) => {
 	const img = document.createElement('img');
 	img.classList.add('userlink-avatar');
 	img.src = `${mw.config.get('wgScriptPath')}/extensions/Avatar/avatar.php?user=${item.textContent}`;
-	img.alt = `Avatar of ${item.textContent}`; // alt
+	img.alt = ''; // alt="" for decorative images
 	item.prepend(img);
 });