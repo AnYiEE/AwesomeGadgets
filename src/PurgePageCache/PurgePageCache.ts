3c9572a94f6654f9b9a2ad65433039de51fd5c2a
ZoruaFox <zorua@vip.qq.com>
2024-06-02 16:53:49 +0800

    chore(PurgePageCache): remove unused code and event listener

--- src/PurgePageCache/PurgePageCache.ts
+++ src/PurgePageCache/PurgePageCache.ts
@@ -1,5 +1,4 @@
 import {getBody} from 'ext.gadget.Util';
-import {getMessage} from './modules/i18n';
 import {purge} from './modules/purge';
 
 void getBody().then(function purgePageCache($body: JQuery<HTMLBodyElement>): void {
@@ -8,21 +7,6 @@ void getBody().then(function purgePageCache($body: JQuery<HTMLBodyElement>): voi
 	if (wgAction !== 'view' || wgNamespaceNumber < 0) {
 		return;
 	}
-
-	const portletId: 'p-cactions' | 'p-tb' = $body.find('#p-cactions').length ? 'p-cactions' : 'p-tb';
-	const portletElement: HTMLLIElement | null = mw.util.addPortletLink(
-		portletId,
-		getMessage('Purge'),
-		getMessage('PurgeFromServer')
-	);
-	if (!portletElement) {
-		return;
-	}
-
-	(portletElement.querySelector('a') as HTMLAnchorElement).addEventListener('click', (event: MouseEvent): void => {
-		event.preventDefault();
-		void purge(wgPageName);
-	});
 
 	for (const element of $body.find<HTMLAnchorElement>('a[href*="action=purge"]')) {
 		const title: string = mw.util.getParamValue('title', element.href) || wgPageName;
